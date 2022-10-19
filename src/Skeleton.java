bb6be6c21cf123382513e53bca76ed99bcc809d1
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-19 20:54:03 +0600

    Update Skeleton.java

--- src/Skeleton.java
+++ src/Skeleton.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Skeleton {
+public class Skeleton extends Boss{
+    private static int arrows;
+
+    public static int getArrows() {
+        return arrows;
+    }
+
+    public static void setArrows(int arrows) {
+        Skeleton.arrows = arrows;
+    }
+
+    public Skeleton(int hp, int damage, Weapon weapon, int arrows) {
+        super(hp, damage, weapon);
+        this.arrows = arrows;
+    }
+    public  String printInfo(){
+        return "Boss HP: "+ Boss.getHp() +" "+ "Boss damage"+ Boss.getDamage()+" "+"Boss Weapon: "
+                + Boss.getWeapon().getWeaponType() +" "+ "Boss Weapon Name: " + Boss.getWeapon().getWeaponName() +" "+ "Arrows: " +
+                arrows;
+
+    }
 }
