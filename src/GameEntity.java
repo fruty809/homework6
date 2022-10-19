207d810d938a2449a641ededaa9a2e727705fefd
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-19 20:53:42 +0600

    Update GameEntity.java

--- src/GameEntity.java
+++ src/GameEntity.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class GameEntity {
+public class GameEntity {
+    private static int hp;
+    private static int damage;
+
+    public static int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public static int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public GameEntity(int hp, int damage) {
+        this.hp = hp;
+        this.damage = damage;
+
+
+    }
 }
