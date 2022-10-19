96ffdef483feee3fa934f3a4759470c328104498
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-19 20:54:37 +0600

    Update Weapon.java

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Weapon {
+public class Weapon {
+
+    private static WeaponType weaponType;
+    private String weaponName;
+
+    public WeaponType getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(WeaponType weaponType) {
+        this.weaponType = weaponType;
+    }
+
+    public String getWeaponName() {
+        return weaponName;
+    }
+
+    public void setWeaponName(String weaponName) {
+        this.weaponName = weaponName;
+    }
+
+    public Weapon(WeaponType weaponType, String weaponName) {
+        this.weaponType = weaponType;
+        this.weaponName = weaponName;
+
+    }
 }
