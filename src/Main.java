d2c0269a8bb6b96bc470fc429a0d92783219fd13
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-19 20:56:38 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Boss boss1;
-        boss1 = new Boss(500,40, new Weapon(WeaponType.MAGIC,"Stick"));
+        boss1 = new Boss(550,50, new Weapon(WeaponType.MAGIC,"Stick"));
         System.out.println(boss1.printInfo());
-        Skeleton skeleton1 = new Skeleton(100,5, new Weapon(WeaponType.MAGIC,"bow"),100);
+        Skeleton skeleton1 = new Skeleton(120,10, new Weapon(WeaponType.MAGIC,"bow"),110);
         System.out.println(skeleton1.printInfo());
-        Skeleton skeleton2 = new Skeleton(250,10, new Weapon(WeaponType.GUNSHOT,"CrossBow"),100);
+        Skeleton skeleton2 = new Skeleton(275,15, new Weapon(WeaponType.GUNSHOT,"CrossBow"),100);
         System.out.println(skeleton2.printInfo());
     }
 }
