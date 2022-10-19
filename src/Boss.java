249e8f6767249b08a6d352f21546ac2ac3e4f776
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-19 20:53:07 +0600

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Boss {
+public class Boss extends GameEntity{
+    private static Weapon weapon;
+
+    public static Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+    public Boss(int hp, int damage, Weapon weapon) {
+        super(hp, damage);
+
+        this.weapon = weapon;
+
+    }
+    public String printInfo(){
+        return "Boss HP: "+ Boss.getHp() +"|"+ "Boss damage"+ Boss.getDamage()+"|"+"Boss Weapon: "
+                + Boss.getWeapon().getWeaponType() +"|"+ "Boss Weapon Name: " + Boss.getWeapon().getWeaponName();
+
+    }
 }
